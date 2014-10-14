930c51c85f2de193f836bb35d73ed6e124549de3
Karel Liiv <kareliiv@gmail.com>
2014-10-14 15:35:18 +0300

    Set default language automatically

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import argparse
+import locale
 import sys
 from query import Query
 
 if __name__ == "__main__":
+    default_lang = locale.getdefaultlocale()[0][:2]
+
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", "--file", required=True, help="Name or file to search")
     parser.add_argument("-c", "--count", type=int, default=1, help="Maximum number of files to download")
-    parser.add_argument("-l", "--lang", help="Language of the subtitles")
+    parser.add_argument("-l", "--lang", default=default_lang, help="Language of the subtitles")
 
     args = parser.parse_args()
     query = None
