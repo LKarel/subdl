bc76ea802495920b90997b936dd635cb7745efb6
Karel Liiv <kareliiv@gmail.com>
2014-10-14 13:21:52 +0300

    Argumendi puudumisel veateade
    Kui puudub argument siis v2ljastatakse "Missing name"

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from query import Query
 if __name__ == "__main__":
     query = None
 
+    if len(sys.argv) < 2:
+        print("Missing name")
+        sys.exit(1)
+
     if False:
         # If query is file, extract the query info from there
         pass
