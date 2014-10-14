026d24d8da8cd622afab2ef2a8c41c341c65c4cb
Ragnis Armus <ragnis.armus@gmail.com>
2014-10-14 15:22:01 +0300

    Set '-f' as required

--- main.py
+++ main.py
@@ -4,16 +4,12 @@ from query import Query
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-f", "--file", help="Name or file to search")
+    parser.add_argument("-f", "--file", required=True, help="Name or file to search")
     parser.add_argument("-c", "--count", type=int, default=1, help="Maximum number of files to download")
     parser.add_argument("-l", "--lang", help="Language of the subtitles")
 
     args = parser.parse_args()
     query = None
-
-    if "file" not in args:
-        print("Missing name argument")
-        sys.exit(1)
 
     if False:
         # If query is file, extract the query info from there
